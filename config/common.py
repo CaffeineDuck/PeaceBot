464b2cd42ab45d3ca9bfaa442c651b7285b659e5
Samrid Pandit <samrid@pop-os.localdomain>
2021-03-04 00:49:49 +0545

    Fixed some things for heroku

--- config/common.py
+++ config/common.py
@@ -1,9 +1,15 @@
+import ssl
+
 from pydantic import BaseSettings, PostgresDsn
 
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 class Settings(BaseSettings):
 
-    database_uri: PostgresDsn
+    database_url: PostgresDsn
+    secret: str
 
     class Config:
         env_file = ".env"
