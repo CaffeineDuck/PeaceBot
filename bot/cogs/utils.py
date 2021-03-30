d0d2cfef4d959ae7de97b2aeb0bd29c38272740a
Samrid Pandit <samrid@pop-os.localdomain>
2021-03-30 13:12:27 +0545

    Added the embed command from https://github.com/falsedev/tech-struck

--- bot/cogs/utils.py
+++ bot/cogs/utils.py
@@ -1,11 +1,30 @@
+import re
+import sys
 from typing import Optional
 
 import discord
-from discord.ext import commands
+from discord import Embed, Message, TextChannel
+from discord.ext import commands, flags
 
+from __main__ import PeaceBot
+from utils.embed_flag_input import (
+    allowed_mentions_input,
+    dict_to_allowed_mentions,
+    dict_to_embed,
+    embed_input,
+    process_message_mentions,
+    webhook_input,
+)
+
+flags._converters.CONVERTERS["Message"] = commands.MessageConverter().convert
+
+async def maybe_await(coro):
+    if not coro:
+        return
+    return await coro
 
 class Utils(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: PeaceBot):
         self.bot = bot
 
     @commands.command(name="avatar", aliases=["av"])
@@ -22,5 +41,103 @@ class Utils(commands.Cog):
         await ctx.send(embed=embed)
 
 
-def setup(bot: commands.Bot):
+
+    """
+    Source of the following code:
+    https://github.com/falsedev/tech-struck
+    """
+    @embed_input(all=True)
+    @allowed_mentions_input()
+    @webhook_input()
+    @flags.add_flag("--channel", "--in", type=TextChannel, default=None)
+    @flags.add_flag("--message", "--msg", "-m", default=None)
+    @flags.add_flag("--edit", "-e", type=Message, default=None)
+    @flags.command(
+        brief="Send an embed with any fields, in any channel, with command line like arguments"
+    )
+    @commands.has_guild_permissions(administrator=True)
+    @commands.bot_has_permissions(manage_webhooks=True, embed_links=True)
+    async def embed(self, ctx: commands.Context, **kwargs):
+        """
+        Send an embed and its fully customizable
+        Default mention settings:
+            Users:      Enabled
+            Roles:      Disabled
+            Everyone:   Disabled
+        """
+        embed = dict_to_embed(kwargs, author=ctx.author)
+        allowed_mentions = dict_to_allowed_mentions(kwargs)
+        message = process_message_mentions(kwargs.pop("message"))
+
+        if kwargs.pop("webhook"):
+            if (edit_message := kwargs.pop("edit")) :
+                edit_message.close()
+            username, avatar_url = kwargs.pop("webhook_username"), kwargs.pop(
+                "webhook_avatar"
+            )
+            if kwargs.pop("webhook_auto_author"):
+                username, avatar_url = (
+                    username or ctx.author.display_name,
+                    avatar_url or ctx.author.avatar_url,
+                )
+            target = kwargs.pop("channel") or ctx.channel
+            if (name := kwargs.pop("webhook_new_name")) :
+                wh = await target.create_webhook(name=name)
+            elif (name := kwargs.pop("webhook_name")) :
+                try:
+                    wh = next(
+                        filter(
+                            lambda wh: wh.name.casefold() == name.casefold(),
+                            await target.webhooks(),
+                        )
+                    )
+                except StopIteration:
+                    return await ctx.send(
+                        "No pre existing webhook found with given name"
+                    )
+            else:
+                return await ctx.send("No valid webhook identifiers provided")
+            await wh.send(
+                message,
+                embed=embed,
+                allowed_mentions=allowed_mentions,
+                username=username,
+                avatar_url=avatar_url,
+            )
+            if kwargs.pop("webhook_dispose"):
+                await wh.delete()
+            return await ctx.message.add_reaction("\u2705")
+
+        if (edit := await maybe_await(kwargs.pop("edit"))) :
+            if edit.author != ctx.guild.me:
+                return await ctx.send(
+                    f"The target message wasn't sent by me! It was sent by {edit.author}"
+                )
+            await edit.edit(
+                content=message, embed=embed, allowed_mentions=allowed_mentions
+            )
+        else:
+            target = kwargs.pop("channel") or ctx
+            await target.send(message, embed=embed, allowed_mentions=allowed_mentions)
+        await ctx.message.add_reaction("\u2705")
+
+    @commands.command()
+    async def rawembed(self, ctx: commands.Context):
+        ref = ctx.message.reference
+        if not ref or not ref.message_id:
+            return await ctx.send("Reply to an message with an embed")
+        message = ref.cached_message or await ctx.channel.fetch_message(ref.message_id)
+
+        if not message.embeds:
+            return await ctx.send("Message had no embeds")
+        em = message.embeds[0]
+        description = "```" + str(em.to_dict()) + "```"
+        embed = Embed(description=description)
+        await ctx.reply(embed=embed)
+
+
+def teardown(bot: PeaceBot):
+    del sys.modules["bot.utils.embed_flag_input"]
+
+def setup(bot: PeaceBot):
     bot.add_cog(Utils(bot))
---
+++ utils/embed_flag_input.py
@@ -0,0 +1,220 @@
+"""
+Source of the following code:
+https://github.com/falsedev/tech-struck
+"""
+
+import functools
+import re
+from typing import Dict, Iterable, TypeVar, Union
+from urllib import parse
+
+from discord import AllowedMentions, Embed, Member, User
+from discord.ext import commands, flags  # type: ignore
+
+_F = TypeVar(
+    "_F",
+)
+
+
+class InvalidFieldArgs(commands.CommandError):
+    pass
+
+
+class EmbeyEmbedError(commands.CommandError):
+    def __str__(self) -> str:
+        return "The embed has no fields/attributes populated"
+
+
+class InvalidUrl(commands.CommandError):
+    def __init__(self, invalid_url: str, *, https_only: bool = False) -> None:
+        self.invalid_url = invalid_url
+        self.https_only = https_only
+
+    def __str__(self) -> str:
+        return "The url entered (`%s`) is invalid.%s" % (
+            self.invalid_url,
+            "\nThe url must be https" if self.https_only else "",
+        )
+
+
+class InvalidColor(commands.CommandError):
+    def __init__(self, value) -> None:
+        self.value = value
+
+    def __str__(self):
+        return f"{self.value} isn't a valid color, eg: `#fff000`, `f0f0f0`"
+
+
+class UrlValidator:
+    def __init__(self, *, https_only=False) -> None:
+        self.https_only = https_only
+
+    def __call__(self, value):
+        url = parse.urlparse(value)
+        schemes = ("https",) if self.https_only else ("http", "https")
+        if url.scheme not in schemes or not url.hostname:
+            raise InvalidUrl(value, https_only=self.https_only)
+        return value
+
+
+def colortype(value: str):
+    try:
+        return int(value.removeprefix("#"), base=16)
+    except ValueError or AttributeError:
+        raise InvalidColor(value)
+
+
+url_type = UrlValidator(https_only=True)
+
+
+def process_message_mentions(message: str) -> str:
+    if not message:
+        return ""
+    for _type, _id in re.findall(r"(role|user):(\d{18})", message):
+        message = message.replace(
+            _type + ":" + _id, f"<@!{_id}>" if _type == "user" else f"<@&{_id}>"
+        )
+    for label in ("mention", "ping"):
+        for role in ("everyone", "here"):
+            message = message.replace(label + ":" + role, f"@{role}")
+    return message
+
+
+class FlagAdder:
+    def __init__(self, kwarg_map: Dict[str, Iterable], *, default_mode: bool = False):
+        self.kwarg_map = kwarg_map
+        self.default_mode = default_mode
+
+    def call(self, func: _F, **kwargs) -> _F:
+        if kwargs.pop("all", False):
+            for flags in self.kwarg_map.values():
+                self.apply(flags=flags, func=func)
+            return func
+        kwargs = {**{k: self.default_mode for k in self.kwarg_map.keys()}, **kwargs}
+        for k, v in kwargs.items():
+            if v:
+                self.apply(flags=self.kwarg_map[k], func=func)
+        return func
+
+    def __call__(self, func=None, **kwargs):
+        if func is None:
+            return functools.partial(self.call, **kwargs)
+        return self.call(func, **kwargs)
+
+    def apply(self, *, flags: Iterable, func: _F) -> _F:
+        for flag in flags:
+            flag(func)
+        return func
+
+
+embed_input = FlagAdder(
+    {
+        "basic": (
+            flags.add_flag("--title", "-t"),
+            flags.add_flag("--description", "-d"),
+            flags.add_flag("--fields", "-f", nargs="+"),
+            flags.add_flag("--colour", "--color", "-c", type=colortype),
+        ),
+        "image": (
+            flags.add_flag("--thumbnail", "-th", type=url_type),
+            flags.add_flag("--image", "-i", type=url_type),
+        ),
+        "author": (
+            flags.add_flag("--author-name", "--aname", "-an"),
+            flags.add_flag("--auto-author", "-aa", action="store_true", default=False),
+            flags.add_flag("--author-url", "--aurl", "-au", type=url_type),
+            flags.add_flag("--author-icon", "--aicon", "-ai", type=url_type),
+        ),
+        "footer": (
+            flags.add_flag("--footer-icon", "-fi", type=url_type),
+            flags.add_flag("--footer-text", "-ft"),
+        ),
+    }
+)
+
+
+allowed_mentions_input = FlagAdder(
+    {
+        "all": (
+            flags.add_flag(
+                "--everyone-mention", "-em", default=False, action="store_true"
+            ),
+            flags.add_flag(
+                "--role-mentions", "-rm", default=False, action="store_true"
+            ),
+            flags.add_flag(
+                "--user-mentions", "-um", default=True, action="store_false"
+            ),
+        )
+    },
+    default_mode=True,
+)
+
+webhook_input = FlagAdder(
+    {
+        "all": (
+            flags.add_flag("--webhook", "-w", action="store_true", default=False),
+            flags.add_flag("--webhook-username", "-wun", type=str, default=None),
+            flags.add_flag("--webhook-avatar", "-wav", type=url_type, default=None),
+            flags.add_flag(
+                "--webhook-auto-author", "-waa", action="store_true", default=False
+            ),
+            flags.add_flag("--webhook-new-name", "-wnn", type=str, default=None),
+            flags.add_flag("--webhook-name", "-wn", type=str, default=None),
+            flags.add_flag(
+                "--webhook-dispose", "-wd", action="store_true", default=False
+            ),
+        )
+    },
+    default_mode=True,
+)
+
+
+def dict_to_embed(data: Dict[str, str], author: Union[User, Member] = None):
+    embed = Embed()
+    for field in ("title", "description", "colour"):
+        if (value := data.pop(field, None)) :
+            setattr(embed, field, value)
+    for field in "thumbnail", "image":
+        if (value := data.pop(field, None)) :
+            getattr(embed, "set_" + field)(url=value)
+
+    if data.pop("auto_author", False) and author:
+        embed.set_author(name=author.display_name, icon_url=str(author.avatar_url))
+    if "author_name" in data and data["author_name"]:
+        kwargs = {}
+        if (icon_url := data.pop("author_icon", None)) :
+            kwargs["icon_url"] = icon_url
+        if (author_url := data.pop("author_url", None)) :
+            kwargs["url"] = author_url
+
+        embed.set_author(name=data.pop("author_name"), **kwargs)
+
+    if "footer_text" in data and data["footer_text"]:
+        kwargs = {}
+        if (footer_icon := data.pop("footer_icon", None)) :
+            kwargs["icon_url"] = footer_icon
+
+        embed.set_footer(text=data.pop("footer_text"), **kwargs)
+
+    fields = data.pop("fields", []) or []
+    if len(fields) % 2 == 1:
+        raise InvalidFieldArgs(
+            "Number of arguments for fields must be an even number, pairs of name and value"
+        )
+
+    for name, value in zip(fields[::2], fields[1::2]):
+        embed.add_field(name=name, value=value, inline=False)
+
+    if embed.to_dict() == {"type": "rich"}:
+        raise EmbeyEmbedError()
+
+    return embed
+
+
+def dict_to_allowed_mentions(data):
+    return AllowedMentions(
+        everyone=data.pop("everyone_mention"),
+        roles=data.pop("role_mentions"),
+        users=data.pop("user_mentions"),
+    )
