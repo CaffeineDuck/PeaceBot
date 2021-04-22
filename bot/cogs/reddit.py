4498d59b9038ce595c621bc2e3c5248ada5ce3fe
Samrid Pandit <samrid@pop-os.localdomain>
2021-04-22 21:06:48 +0545

    Added meme, aww, cursed, commands

---
+++ bot/cogs/reddit.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+from bot.bot import PeaceBot
+from bot.utils.cached_reddit import RedditPostCacher
+
+
+class Reddit(commands.Cog):
+    def __init__(self, bot: PeaceBot):
+        self.bot = bot
+        self.subreddits = ("aww", "memes", "cursedcomments")
+        self.cache = RedditPostCacher(self.subreddits, "cache/Reddit.pickle")
+        self.cache.cache_posts.start()
+
+    @commands.command(aliases=["memes"])
+    async def meme(self, ctx: commands.Context):
+        await self.cache.reddit_sender(ctx, "memes")
+
+    @commands.command()
+    async def aww(self, ctx: commands.Context):
+        await self.cache.reddit_sender(ctx, "aww")
+
+    @commands.command(aliases=['cursedcomments'])
+    async def cursed(self, ctx: commands.Context):
+        await self.cache.reddit_sender(ctx, "cursedcomments")
+
+def setup(bot: PeaceBot):
+    bot.add_cog(Reddit(bot))
