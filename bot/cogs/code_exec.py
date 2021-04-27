6d0a7c13fabde58c578abd55626baf6229b3b9c1
Samrid Pandit <samrid@pop-os.localdomain>
2021-04-27 19:05:22 +0545

    Black formatting

--- bot/cogs/code_exec.py
+++ bot/cogs/code_exec.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from config.bot import bot_config
 from bot.bot import PeaceBot
 
+
 class CodeExec(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
