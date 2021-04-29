dc42dd9d26bbcfd85bab19e15552164306422742
Samrid Pandit <samrid@pop-os.localdomain>
2021-04-29 11:32:46 +0545

    Added some perms checks for changeprefix command

--- bot/cogs/config.py
+++ bot/cogs/config.py
@@ -21,7 +21,7 @@ class Config(commands.Cog):
 
     @commands.command()
     @commands.cooldown(1, 10, BucketType.user)
-    @commands.bot_has_permissions(send_messages=True, read_messages=True)
+    @commands.has_permissions(manage_guild=True)
     @commands.guild_only()
     async def changeprefix(self, ctx: commands.Context, prefix: str):
         guild = await GuildModel.from_context(ctx)
