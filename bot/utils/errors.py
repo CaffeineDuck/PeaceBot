4584fda6e17a3de4a18fefc49530c93603ebfe44
Samrid Pandit <samrid@pop-os.localdomain>
2021-05-02 14:43:50 +0545

    Made the CommandDisabled error a bit more explicit

--- bot/utils/errors.py
+++ bot/utils/errors.py
@@ -2,5 +2,9 @@ from discord.ext.commands import CheckFailure
 
 
 class CommandDisabled(CheckFailure):
+    def __init__(self, command: str, cog: str):
+        self.command = command
+        self.cog = cog
+
     def __str__(self):
-        return "This command has been disabled in this channel!"
+        return f"The command `{self.command}` or its command group {self.cog} has been disabled in this channel!"
