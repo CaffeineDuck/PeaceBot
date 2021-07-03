0541781ea2deab1944895e00d9c5bbe34520fe51
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-03 21:37:06 +0545

    Passed error logging webhook url to bot.p

--- bot/__main__.py
+++ bot/__main__.py
@@ -11,11 +11,13 @@ os.environ.setdefault("JISHAKU_NO_UNDERSCORE", "1")
 if __name__ == "__main__":
     from config.bot import bot_config
     from tortoise_config import tortoise_config
+    from config.misc import misc_settings
 
     bot = PeaceBot(
         tortoise_config=tortoise_config,
         prefix=bot_config.prefix,
         developement_environment=bot_config.developement_environment,
+        log_webhook_url=misc_settings.log_webhook_url
     )
 
     bot.run(bot_config.token)
