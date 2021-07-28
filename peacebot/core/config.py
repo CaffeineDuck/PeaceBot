fb8a65ce601ce31b07552dde8cb9828d41f52e8f
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-28 23:13:19 +0545

    [add] Adds `config.py` with config models

---
+++ peacebot/core/config.py
@@ -0,0 +1,25 @@
+from ipaddress import IPv4Address
+from pathlib import Path
+from typing import Optional, Sequence
+
+from pydantic import BaseModel, HttpUrl
+
+from peacebot.core.types import VoiceRegions
+
+
+class LavalinkConfig(BaseModel):
+    host: IPv4Address
+    port: int
+    rest_url: HttpUrl
+    idetifier: str
+    region: VoiceRegions
+
+
+class BotConfig(BaseModel):
+    prefix: str
+    description = 'A simple and shitty discord bot'
+    cogs: Optional[Sequence[str]]
+    cogs_dir: Optional[Path]
+    lavalink_config: Optional[LavalinkConfig]
+    db_config: Optional[dict]
+    private_bot: Optional[bool] = False
