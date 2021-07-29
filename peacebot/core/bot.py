1d0127a0ca7ff1d9da7f660465a91953b9dddc68
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-29 18:39:32 +0545

    [update] Bot now connects to db on startup

--- peacebot/core/bot.py
+++ peacebot/core/bot.py
@@ -2,20 +2,22 @@
 
 import asyncio
 import logging
+from typing import Optional
 
 import discord
 from cachetools import TTLCache
 from discord.ext import commands
+from tortoise import Tortoise
 
 from peacebot.core.helpers.config import BotConfig, LavalinkConfig
 
 logger = logging.getLogger("peacebot.main")
-
+logging.basicConfig(level=logging.INFO)
 
 class PeaceBot(commands.Bot):
     """This is the core `PeaceBot`, the main bot"""
 
-    def __init__(self, config: BotConfig):
+    def __init__(self, config: BotConfig, tortoise_config: Optional[dict] = None):
         # Passes the args to the commands.Bot's init
         super().__init__(
             command_prefix=config.prefix,
@@ -24,9 +26,7 @@ class PeaceBot(commands.Bot):
 
         # Config object
         self.config = config
-
-        # Locks the bot from handling on_message events
-        self.lock_bot = True
+        self.tortoise_config = tortoise_config
 
         # Checks and connects to lavalink/DB according to config
         self._config_checker(self.config)
@@ -35,20 +35,31 @@ class PeaceBot(commands.Bot):
         self._guild_model_cache = TTLCache(100, 1000)
         self._user_model_cache = TTLCache(100, 1000)
 
+    @property
+    def event_loop(self) -> asyncio.BaseEventLoop:
+        return asyncio.get_event_loop()
+
     def _config_checker(self, config: BotConfig) -> None:
         """Checks config and handles the bot according to config"""
 
         if config.db_config:
-            asyncio.create_task(self._connect_db(config.db_config))
+            # Locks the bot from handling on_message events
+            self.lock_bot = True
+            self.event_loop.create_task(self._connect_db(self.tortoise_config))
 
         if config.lavalink_config:
-            asyncio.create_task(self._connect_lavalink(config.lavalink_config))
+            self.event_loop.create_task(self._connect_db(self.tortoise_config))
 
         if config.load_jishaku:
             self.load_extension("jishaku")
 
-    async def _connect_db(self, db_config: dict) -> None:
+    async def _connect_db(self, tortoise_config: dict) -> None:
         """Connects to the postresql database"""
+        if not tortoise_config:
+            raise ValueError("Tortoise config must be passed")
+
+        logger.info("Connecting to database")
+        await Tortoise.init(tortoise_config)
         logger.info("Connected to database")
         self.lock_bot = False
 
@@ -69,3 +80,4 @@ class PeaceBot(commands.Bot):
 
     async def on_message(self, message: discord.Message) -> None:
         """Handles the `messages` for further processing"""
+        await self.process_commands(message)
