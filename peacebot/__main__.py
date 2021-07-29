fca05742586d8acc99ef67692ed3a5344bd401c8
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-29 18:38:46 +0545

    [add] Adds `__main__.py` for running the bot

---
+++ peacebot/__main__.py
@@ -0,0 +1,22 @@
+import os
+
+os.environ.setdefault("JISHAKU_HIDE", "1")
+os.environ.setdefault("JISHAKU_RETAIN", "1")
+os.environ.setdefault("JISHAKU_NO_UNDERSCORE", "1")
+
+from .core import PeaceBot
+from .core.helpers import BotConfig
+from .core.tortoise_config import tortoise_config
+from .env import peacebot_config, peacebot_db_config, peacebot_lavalink_config
+
+bot_config = BotConfig(
+    prefix=peacebot_config.prefix,
+    lavalink_config=peacebot_lavalink_config,
+    db_config=peacebot_db_config,
+    token=peacebot_config.token,
+)
+
+bot = PeaceBot(config=bot_config, tortoise_config=tortoise_config)
+
+if __name__ == '__main__':
+    bot.run(bot_config.token)
