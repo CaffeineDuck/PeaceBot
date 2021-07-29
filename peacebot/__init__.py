6c1c89356a14e34b1e12ed98eb9eb2704755531d
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-29 17:20:37 +0545

    [add] Imports Peacebot

---
+++ peacebot/__init__.py
@@ -0,0 +1,3 @@
+"""This is the core module for accessing using and accessing the bot"""
+
+from .core import PeaceBot
---
+++ peacebot/core/__init__.py
@@ -0,0 +1,3 @@
+"""This is the init module for core"""
+
+from .bot import PeaceBot
