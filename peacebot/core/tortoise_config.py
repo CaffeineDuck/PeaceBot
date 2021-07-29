3bc7ee9547b8de0974355e32311a18641ffc76fb
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-29 18:40:10 +0545

    [add] Creates `tortoise_config.py` for connecting to database

---
+++ peacebot/core/tortoise_config.py
@@ -0,0 +1,21 @@
+from ..env import peacebot_db_config
+
+db_config = peacebot_db_config
+
+tortoise_config = {
+    "connections": {
+        "default": {
+            "engine": "tortoise.backends.asyncpg",
+            "credentials": {
+                "database": db_config.db,
+                "host": db_config.host,
+                "password": db_config.password,
+                "port": db_config.port,
+                "user": db_config.user,
+            },
+        }
+    },
+    "apps": {
+        "main": {"models": ["peacebot.core.models", "aerich.models"], "default_connection": "default"}
+    },
+}
