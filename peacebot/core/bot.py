b59348c4f622ac5732c28266bf5eb10297bbec6a
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-30 03:29:41 +0545

    [add] Adds functionality to `connect_wavelink` method

--- peacebot/core/bot.py
+++ peacebot/core/bot.py
@@ -5,14 +5,17 @@ import logging
 from typing import Optional
 
 import discord
+import wavelink
 from cachetools import TTLCache
 from discord.ext import commands
 from tortoise import Tortoise
 
-from peacebot.core.helpers.config import BotConfig, LavalinkConfig
+from .helpers.config import BotConfig, LavalinkConfig
+from .models import GuildModel
 
 logger = logging.getLogger("peacebot.main")
 logging.basicConfig(level=logging.INFO)
+
 
 class PeaceBot(commands.Bot):
     """This is the core `PeaceBot`, the main bot"""
@@ -23,6 +26,9 @@ class PeaceBot(commands.Bot):
             command_prefix=config.prefix,
             description=config.description,
         )
+
+        # lock_bot doesn't recieve message until its False
+        self.lock_bot = False
 
         # Config object
         self.config = config
@@ -35,8 +41,15 @@ class PeaceBot(commands.Bot):
         self._guild_model_cache = TTLCache(100, 1000)
         self._user_model_cache = TTLCache(100, 1000)
 
+        # Wavelink Client
+        self.wavelink_client = wavelink.Client(bot=self)
+
     @property
     def event_loop(self) -> asyncio.BaseEventLoop:
+        """
+        This returns the existing event loop
+        or creates a event loop and returns it
+        """
         return asyncio.get_event_loop()
 
     def _config_checker(self, config: BotConfig) -> None:
@@ -48,7 +61,7 @@ class PeaceBot(commands.Bot):
             self.event_loop.create_task(self._connect_db(self.tortoise_config))
 
         if config.lavalink_config:
-            self.event_loop.create_task(self._connect_db(self.tortoise_config))
+            self.event_loop.create_task(self._connect_wavelink(config.lavalink_config))
 
         if config.load_jishaku:
             self.load_extension("jishaku")
@@ -63,21 +76,53 @@ class PeaceBot(commands.Bot):
         logger.info("Connected to database")
         self.lock_bot = False
 
-    async def _connect_lavalink(self, lavalink_config: LavalinkConfig) -> None:
-        """Connects to the lavalink music server"""
-        logger.info("Connected to lavalink server")
+    async def _connect_wavelink(self, lavalink_config: LavalinkConfig) -> None:
+        """Connects to the wavelink nodes"""
+        nodes = {
+            lavalink_config.identifier: {
+                "host": lavalink_config.host,
+                "port": lavalink_config.port,
+                "password": lavalink_config.password,
+                "identifier": lavalink_config.identifier,
+                "region": lavalink_config.region,
+                "rest_uri": lavalink_config.rest_url,
+            }
+        }
+        [await self.wavelink_client.initiate_node(**node) for node in nodes.values()]
+        logger.info("Connected to wavelink nodes")
 
-    async def _determine_prefix(
-        self, bot: commands.Bot, message: discord.Message
-    ) -> str:
+    async def _determine_prefix(self, _: commands.Bot, message: discord.Message) -> str:
         """Get the prefix for each command invokation"""
+        if not message.guild:
+            return self.config.prefix
 
-    async def get_local_guild(self, guild_id: int) -> None:
+        guild_model = await self.get_local_guild(message.guild.id)
+        return guild_model.prefix
+
+    async def get_local_guild(self, guild_id: int) -> GuildModel:
         """Get the Guild Model in the local database"""
+        guild_model = self._guild_model_cache.get(guild_id)
+
+        if not guild_model:
+            guild_model, _ = await GuildModel.get_or_create(id=guild_id)
+            self._guild_model_cache[guild_id] = guild_model
+
+        return guild_model
 
     async def get_local_user(self, user_id: int) -> None:
         """Get the User Model in the local database"""
 
     async def on_message(self, message: discord.Message) -> None:
         """Handles the `messages` for further processing"""
-        await self.process_commands(message)
+
+        if self.lock_bot:
+            return
+
+        if f"<@!{self.user.id}>" == message.content.strip():
+            return await message.reply(
+                "My prefix here is `{}`".format(
+                    await self._determine_prefix(None, message)
+                )
+            )
+
+        self.process_commands(message)
