4d224b0c767a5c0b16cf76a26048cf98199253ee
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-30 03:30:18 +0545

    [update] Connects to DB but doesn't connect to lavalink server for now

--- peacebot/__main__.py
+++ peacebot/__main__.py
@@ -16,8 +16,8 @@ os.environ.setdefault("JISHAKU_NO_UNDERSCORE", "1")
 
 bot_config = BotConfig(
     prefix=peacebot_config.prefix,
-    lavalink_config=peacebot_lavalink_config,
-    # db_config=peacebot_db_config,
+    # lavalink_config=peacebot_lavalink_config,
+    db_config=peacebot_db_config,
     token=peacebot_config.token,
 )
 
