827af985abae9c853b43ce99041237199c31b8e2
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-29 17:22:28 +0545

    [move] Moves to `/helpers`

---
+++ peacebot/core/helpers/types.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class VoiceRegions(Enum):
+    AMSTERDAM = "amseterdam"
+    BRAZIL = "brazil"
+    EU_WEST = "eu_west"
+    INDIA = "india"
+    JAPAN = "japan"
+    LONDON = "london"
+    HONGKONG = "hongkong"
+    EU_CENTRAL = "eu_central"
+    DUBAI = "dubai"
+    US_CENTRAL = "us_central"
+    US_EAST = "us_east"
+    US_SOUTH = "us_south"
+    US_WEST = "us_west"
+    RUSSIA = "russia"
+    SINGAPORE = "singapore"
+    SYDNEY = "sydney"
+    SOUTHAFRICA = "southafrica"
+    SOUTH_KOREA = "south_korea"
