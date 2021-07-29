91bfb2aab0ef3bc611358e2a420b9461d1c5fbeb
Samrid Pandit <samrid.pandit@gmail.com>
2021-07-29 18:41:12 +0545

    [patch] `DatabaseConfig` now inherits `pydantic.BaseModel`

--- peacebot/core/helpers/config.py
+++ peacebot/core/helpers/config.py
@@ -1,26 +1,35 @@
 from ipaddress import IPv4Address
 from pathlib import Path
-from typing import Optional, Sequence
+from typing import Literal, Optional, Sequence, Union
 
 from pydantic import BaseModel, HttpUrl
 
 from .types import VoiceRegions
 
 
+class DatabaseConfig(BaseModel):
+    db: str
+    host: Union[IPv4Address, Literal["localhost"]]
+    password: str
+    port: int = 5432
+    user: str
+
+
 class LavalinkConfig(BaseModel):
-    host: IPv4Address
+    host: Union[IPv4Address, Literal["localhost"]]
     port: int
-    rest_url: HttpUrl
-    idetifier: str
-    region: VoiceRegions
+    rest_url: str
+    idetifier: str = "MAIN"
+    region: VoiceRegions = VoiceRegions.INDIA
 
 
 class BotConfig(BaseModel):
     prefix: str
-    description = "A simple and shitty discord bot"
+    token: str
     cogs: Optional[Sequence[str]]
     cogs_dir: Optional[Path]
     lavalink_config: Optional[LavalinkConfig]
-    db_config: Optional[dict]
-    private_bot: Optional[bool] = False
-    load_jishaku: Optional[bool] = True
+    db_config: Optional[DatabaseConfig]
+    private_bot = False
+    description = "A simple and shitty discord bot"
+    load_jishaku = True
